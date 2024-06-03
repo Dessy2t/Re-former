ad797a1c12a48974a3544ca87d424b14fe720a12
Desmond Yentumi <yentumid7@gmail.com>
2024-06-03 09:14:10 +0000

    new user added

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,8 +1,16 @@
 class UsersController < ApplicationController
     def new
+        @users = User.new
     end
 
     def create
+        #@user = User.new(username: params[:username], email: params[:email], password: params[:password])
+        @user = User.new(user_params)
+        if @user.save
+            redirect_to new_users_path
+        else
+            reder :new, status::unprocessable_entity
+        end
     end
-    
+
 end
